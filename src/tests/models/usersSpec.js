2e9e0a3e463d7dcd12747aab9811ee4c20a197cb
Kelly Woldsethgit config --global user.name Kelly <kelly.woldseth@gmail.com>
2022-06-23 17:35:06 -0700

    fixed access to tests

---
+++ src/tests/models/usersSpec.js
@@ -0,0 +1,40 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const users_1 = require("../../models/users");
+const userInfo = new users_1.UserInfo();
+describe("Testing Users Model", () => {
+    it('should have an index method', () => {
+        expect(userInfo.index).toBeDefined();
+    });
+    it('should have a show method', () => {
+        expect(userInfo.show).toBeDefined();
+    });
+    it('should have a create method', () => {
+        expect(userInfo.create).toBeDefined();
+    });
+    it('create method should add a user', async () => {
+        const newUser = await userInfo.create({
+            id: '1',
+            firstName: 'kelly',
+            lastName: 'woldseth',
+            password: 'testing'
+        });
+        expect(newUser).toEqual({
+            id: '1',
+            firstName: 'kelly',
+            lastName: 'woldseth',
+            password: 'testing'
+        });
+    });
+    it('index method should return a list of users', async () => {
+        const result = await userInfo.index();
+        expect(result).toEqual([
+            {
+                id: "1",
+                firstName: "kelly",
+                lastName: "woldseth",
+                password: "testing"
+            }
+        ]);
+    });
+});
